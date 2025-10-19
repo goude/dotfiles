4678a86cb117d70b3066e84a43d4b127285ef677
Daniel Goude <daniel@goude.se>
2025-10-19 13:43:29 +0200

    minor edit/housekeeping activity

--- home/.config/nvim/lua/plugins/nvim-lint.lua
+++ home/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,5 +1,6 @@
 -- https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-10178217
 -- This below is mainly to get rid of the annoying line length warning in markdown files
+-- Still broken however...
 local HOME = os.getenv("HOME")
 return {
   "mfussenegger/nvim-lint",
@@ -9,6 +10,12 @@ return {
       ["markdownlint-cli2"] = {
         args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
       },
+    },
+
+    -- attempting to turn of markdownlint by forcing use of markdownlint-cli2
+    linters_by_ft = {
+      markdown = { "markdownlint-cli2" },
+      ["markdown.mdx"] = { "markdownlint-cli2" },
     },
   },
 }
